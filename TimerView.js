294eb71526c0bbfa0a3859f977dc098af76a6b3d
Josh Justice <jjustice@bignerdranch.com>
2018-01-17 09:17:15 -0500

    Remove examples

--- TimerView.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { observer } from "mobx-react";
-
-const TimerView = observer(({ appState }) => (
-  <button onClick={appState.reset}>Seconds passed: {appState.timer}</button>
-));
-
-export default TimerView;
--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import AppState from './AppState';
 import CounterStore from './CounterStore';
-import TimerView from './TimerView';
 import CounterView from './CounterView';
 
 const counterStore = new CounterStore();
 
 render(
   <div>
-    <TimerView appState={new AppState()} />
     <CounterView store={counterStore} />
   </div>,
   document.getElementById("root")
