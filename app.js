980fd5dc2d28421574890f563e49683c13195c4d
Josh Justice <jjustice@bignerdranch.com>
2018-01-17 07:10:50 -0500

    Add transform plugin

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Counter from './Counter';
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Counter />,
   document.getElementById('root')
 );
