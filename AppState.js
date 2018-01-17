06a8b16decc1af334b767f50d7411cce00ed1b9b
Josh Justice <jjustice@bignerdranch.com>
2018-01-17 09:05:01 -0500

    Extract AppState into a module

---
+++ AppState.js
@@ -0,0 +1,16 @@
+import { observable, action } from "mobx";
+
+export default class AppState {
+  @observable timer = 0;
+
+  constructor() {
+    setInterval(() => {
+      this.timer += 1;
+    }, 1000);
+  }
+
+  @action.bound
+  reset() {
+    this.timer = 0;
+  }
+}
--- app.js
+++ app.js
@@ -2,21 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { observable, action } from "mobx";
 import { observer } from "mobx-react";
-
-class AppState {
-  @observable timer = 0;
-
-  constructor() {
-    setInterval(() => {
-      this.timer += 1;
-    }, 1000);
-  }
-
-  @action.bound
-  reset() {
-    this.timer = 0;
-  }
-}
+import AppState from './AppState';
 
 const TimerView = observer(({ appState }) => (
   <button onClick={appState.reset}>Seconds passed: {appState.timer}</button>
