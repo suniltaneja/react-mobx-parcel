56e5d76b8e141f3948f6dd785c6f6d2f44e4ecb4
Sunil Taneja <suniltaneja@me.com>
2018-09-27 09:41:02 -0400

    adding a test todo app

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import React from "react";
 import { render } from "react-dom";
 import CounterStore from './CounterStore';
 import CounterView from './CounterView';
+import TodoApp from './Todo';
 
 const counterStore = new CounterStore();
 
 render(
   <div>
     <CounterView store={counterStore} />
+    <TodoApp />
   </div>,
   document.getElementById("root")
 );
